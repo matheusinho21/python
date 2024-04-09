c3f5845853d199479020ae047a64680666c2c10d
projeto 1 <129951535+matheusinho21@users.noreply.github.com>
2024-04-09 10:53:29 -0300

    Create numeros.py

---
+++ numeros.py
@@ -0,0 +1,7 @@
+numeros = [int(input("Digite o primeiro número: ")),
+int(input("Digite o segundo número: ")),
+int(input("Digite o terceiro número: "))]
+
+numeros.sort()
+
+print("Números em ordem crescente:", numeros)
